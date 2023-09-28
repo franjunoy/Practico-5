06ab8e953433a77a17985cc1709dd0ef151e2c68
franjunoy <franjunoy@hotmail.com>
2023-09-28 01:03:12 -0300

    Prettier

--- main.c
+++ main.c
@@ -2,62 +2,78 @@
 #include <stdlib.h>
 #include "listaDob.h"
 
-lista *crearLista(int dato){
-    lista* nuevo= NULL;
-    nuevo = (lista*)malloc(sizeof(lista));
-    if(nuevo!=NULL){
-        nuevo->dato=dato;
-        nuevo->sgte=NULL;
-        nuevo->ante=NULL;
+lista *crearLista(int dato)
+{
+    lista *nuevo = NULL;
+    nuevo = (lista *)malloc(sizeof(lista));
+    if (nuevo != NULL)
+    {
+        nuevo->dato = dato;
+        nuevo->sgte = NULL;
+        nuevo->ante = NULL;
     }
     return nuevo;
 }
 
-int insertar (lista **i, int d){
+int insertar(lista **i, int d)
+{
     lista *nuevo = NULL;
     nuevo = crearLista(d);
-    if(nuevo != NULL){
+    if (nuevo != NULL)
+    {
         nuevo->sgte = *i;
         nuevo->ante = NULL;
-        if(*i != NULL)
+        if (*i != NULL)
             (*i)->ante = nuevo;
-            *i = nuevo;
+        *i = nuevo;
         return 0;
-        }
+    }
     return 1;
 }
-void listar(lista *i){
+void listar(lista *i)
+{
     lista *aux = i;
-    while(aux != NULL){
+    while (aux != NULL)
+    {
         printf("%d\n", aux->dato);
         aux = aux->sgte;
     }
 }
-int insertarFin(lista **i, int dato){
+int insertarFin(lista **i, int dato)
+{
     lista *nuevo = NULL, *aux = *i;
     nuevo = crearLista(dato);
-    if(nuevo != NULL){
-        while(aux->sgte != NULL)(aux = aux->sgte);
+    if (nuevo != NULL)
+    {
+        while (aux->sgte != NULL)
+            (aux = aux->sgte);
         nuevo->ante = aux;
         aux->sgte = nuevo;
         return 0;
     }
     return 1;
 }
-int eliminar(lista **i, int dato){
+int eliminar(lista **i, int dato)
+{
     lista *actual = *i, *ant = NULL, *sig = NULL;
-    while(actual != NULL){
-        if(actual->dato == dato){
-            if(actual == *i){
+    while (actual != NULL)
+    {
+        if (actual->dato == dato)
+        {
+            if (actual == *i)
+            {
                 *i = actual->sgte;
-                if(actual->sgte != NULL)
+                if (actual->sgte != NULL)
                     actual->sgte->ante = NULL;
-            } else
-            if(actual->sgte == NULL){
+            }
+            else if (actual->sgte == NULL)
+            {
                 ant = actual->ante;
                 actual->ante = NULL;
                 ant->sgte = NULL;
-            }else{
+            }
+            else
+            {
                 ant = actual->ante;
                 actual->ante = NULL;
                 sig = actual->sgte;
@@ -72,7 +88,8 @@ int eliminar(lista **i, int dato){
     }
     return 1;
 }
-int main(){
+int main()
+{
     lista *i;
     i = NULL;
     insertar(&i, 5);
