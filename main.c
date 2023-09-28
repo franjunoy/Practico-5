4aaeb871a17aa2ef31828f7af9602ec9429c29df
franjunoy <franjunoy@hotmail.com>
2023-09-28 01:02:16 -0300

    Listar y Fin

--- main.c
+++ main.c
@@ -26,3 +26,21 @@ int insertar (lista **i, int d){
         }
     return 1;
 }
+void listar(lista *i){
+    lista *aux = i;
+    while(aux != NULL){
+        printf("%d\n", aux->dato);
+        aux = aux->sgte;
+    }
+}
+int insertarFin(lista **i, int dato){
+    lista *nuevo = NULL, *aux = *i;
+    nuevo = crearLista(dato);
+    if(nuevo != NULL){
+        while(aux->sgte != NULL)(aux = aux->sgte);
+        nuevo->ante = aux;
+        aux->sgte = nuevo;
+        return 0;
+    }
+    return 1;
+}
